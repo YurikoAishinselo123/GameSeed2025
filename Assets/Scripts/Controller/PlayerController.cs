012ea1d233970852b0f220312fd2a915cbdf5b57
YurikoAishinselo123 <aishinseloyuriko124@gmail.com>
2025-08-04 05:41:49 +0700

    Integrate Player controller with systsem

--- Assets/Scripts/Controller/PlayerController.cs
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -33,13 +33,13 @@ public class PlayerController : MonoBehaviour
         // Flip sprite based on horizontal movement
         if (moveX > 0)
         {
-            DetectionEvents.RaiseDirectionChanged(Vector2.right);
-            spriteRenderer.flipX = false;
+            DetectionEvents.RaiseDirectionChanged(Vector2.left);
+            spriteRenderer.flipX = true;
         }
         else if (moveX < 0)
         {
-            DetectionEvents.RaiseDirectionChanged(Vector2.left);
-            spriteRenderer.flipX = true;
+            DetectionEvents.RaiseDirectionChanged(Vector2.right);
+            spriteRenderer.flipX = false;
         }
     }
 }
